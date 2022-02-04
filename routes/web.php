4949e326917fd0e050ab747f011572c05d93ad91
Bot <89828236+Theodhore@users.noreply.github.com>
2022-02-04 15:09:14 +0700

    theodhore1
    1

--- routes/web.php
+++ routes/web.php
@@ -12,37 +12,23 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('cashier');
+Route::get('/', function(){
+    return view('welcome');
 });
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home')->middleware('auth');
-
-Auth::routes();
 
 
+// Route Untuk Omah Kunci
 
-Route::middleware('auth')->group(function () {
-    Route::get('/kasir', 'CassierController@index')->name('trans');
-    Route::post('/tambah', 'CassierController@tambah');
-    Route::post('/tambahbarang', 'CassierController@tambahdata');
-    Route::post('/hapustrans', 'CassierController@hapustrans');
-    Route::get('kasir/hapus/{id}', 'CassierController@hapus')->name('hapus');
-    Route::get('kasir/hapustrans/{idtrans}', 'CassierController@hapustransaksi')->name('hapustrans');
-    Route::get('/notas', function(){
-        return view('nota.nota');
-    });
-    Route::post('/print', 'CassierController@print')->name('cetak');
-    Route::get('/reload', 'CassierController@reload')->name('reload');
-    Route::get('/transaksi', 'CassierController@show')->name("transaksi");
-    Route::get('/userguide', function(){
-        return view('guide.guide');
-    });
+Route::get('/home', function () {
+    return view('home');
+});
+Route::get('/login', function(){
+    return view('kunci.login');
+});
+Route::get('/kasir', function(){
+    return view('kasir');
+});
+Route::get('transaksi', function(){
+    return view('transaksi');
 });
