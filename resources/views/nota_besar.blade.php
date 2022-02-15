09f936b5679401dd8a854ad1df22cd96abe1df25
Bot <89828236+Theodhore@users.noreply.github.com>
2022-02-15 14:29:48 +0700

    15-2-2022 theodhore
    okeee

---
+++ resources/views/nota_besar.blade.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row d-flex align-items-center justify-content-center mt-5">
+            <img src="{{ asset('assets/Group 1.svg') }}" alt="">
+            <i><b><h1 class="ml-4">Omah Kunci</h1></b></i>
+        </div>
+        <div class="row d-flex align-items-center justify-content-center">
+            <p>Jl. Agus Salim D no.10 <br> Telp/Fax (024) 3554929 <br> Semarang</p>
+        </div>
+
+        <div class="col d-flex align-items-center justify-content-center">
+            <div class="row">
+                <div class="col">
+                    <h2 class="p-0 m-0 text-center">NOTA</h2>
+                    <hr class="p-0 m-0 w-100 bg-dark">
+                    <h5 class="p-0 m-0 mb-2 text-center">NO.000391</h5>
+                </div>
+            </div>
+        </div>
+
+        <br>
+
+        <div class="container ml-5">
+            <div class="row">
+                <h4>Telah terima dari :</h4>
+            </div>
+            <div class="row">
+                <h4>Up : </h4>
+            </div>
+            <div class="row">
+                <h4>Uang Sejumlah : </h4>
+            </div>
+            <br>
+            <div class="row">
+                <h4>Berupa : </h4>
+            </div>
+            <div class="row">
+                <h4>Guna Membayar : </h4>
+            </div>
+            <div class="row">
+                <h4>Total : </h4>
+            </div>
+            <br>
+            <div class="row">
+                <h4>Item Barang : </h4>
+            </div>
+            <div class="row">
+                <h4>Warna/Type : </h4>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="col d-flex align-items-center justify-content-center">
+                <div class="row">
+                    <h4 class="p-0 m-0">Mengetahui,</h4>
+                </div>
+            </div>
+            <hr class="bg-dark w-25">
+        </div>
+    </div>
+</body>
+</html>
